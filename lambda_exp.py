3bf3e5cb91af857f866b8194996756a89d024216
Abhishek kumar manglam <44540425+mangabhi@users.noreply.github.com>
2019-12-20 12:18:37 +0530

    Create lambda_exp.py

---
+++ lambda_exp.py
@@ -0,0 +1,12 @@
+#Write a program to find maximum between two numbers with/without using conditional operator.
+x=lambda a,b:a>b               #taking two variable
+print(x(3,4))
+
+
+or,
+a=int(input("ENter a numner"))
+b=int(input("ENter a numner"))
+if(a>b):
+   print(a)
+else:
+  print(b)
