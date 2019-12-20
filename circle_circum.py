6928e541d71c30d7316cfea955ca62d015e8aa52
Abhishek kumar manglam <44540425+mangabhi@users.noreply.github.com>
2019-12-20 14:28:30 +0530

    Create circle_circum.py

---
+++ circle_circum.py
@@ -0,0 +1,9 @@
+#Write a program to enter radius of a cirle and find its diameter, circumference and area.
+
+r=int(input("Enter a number"))
+d=2*r
+circum=2*22/7*r
+Area=22/7*r*r
+print(d)
+print(circum)
+print(Area)
