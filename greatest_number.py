1d682b9fde8f8a5af78112f1ee4252467b2e39aa
Abhishek kumar manglam <44540425+mangabhi@users.noreply.github.com>
2019-12-20 12:10:14 +0530

    Create greatest_number.py

---
+++ greatest_number.py
@@ -0,0 +1,13 @@
+#Greatest among 3 number
+
+a=int(input("Enter a number"))
+b=int(input("Enter a number"))
+c=int(input("Enter a number"))
+if(a>b)and(a>c):
+    print(a,"is the greatest number")
+elif(b>a)and(b>c):
+    print(b,"is the greatest number")
+else:
+    print(c,"is the greatest number")
+    
+    
