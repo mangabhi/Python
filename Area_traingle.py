fdea11ac785f6268efeccad4548e18773f538f6a
Abhishek kumar manglam <44540425+mangabhi@users.noreply.github.com>
2019-12-23 11:55:27 +0530

    Create Area_traingle.py

---
+++ Area_traingle.py
@@ -0,0 +1,5 @@
+#Write a program to enter base and height of a triangle and find its area.
+base=int(input(" enter base value"))
+height=int(input("enter height value"))
+area=1/2*base*height
+print(area)
