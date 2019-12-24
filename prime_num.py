0697b808d2dcbe777839928ce561a4e4d3aceba3
Abhishek kumar manglam <44540425+mangabhi@users.noreply.github.com>
2019-12-24 21:15:04 +0530

    Update prime_num.py

--- prime_num.py
+++ prime_num.py
@@ -1,10 +1,14 @@
-n=int(input("Enter a number"))
-if n>1:
-    for i in range(2,n):
-        if(n%i==0):
-            print(n,"is not prime number")
-            break;
-    else:
-        print(n,"is a prime number")
-else:
-    print(n,"is a prime number")
+m=int(input("Enter a number"))
+p=int(input("Enter a number"))
+
+for n in range(m,p):
+    if n>1:
+        for i in range(2,n):
+            if(n%i==0):
+                #print(n,"is not prime number")
+                break;
+        else:
+            print(n)
+    
+    
+    
