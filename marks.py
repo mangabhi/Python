7abb9349727139100df4b4dbfdebd68e3dfdac4f
Abhishek kumar manglam <44540425+mangabhi@users.noreply.github.com>
2019-12-24 21:01:06 +0530

    Create marks.py

---
+++ marks.py
@@ -0,0 +1,13 @@
+#Description : Write a program to enter marks of five subjets and calculate total, average and perentage.
+
+a=float(input("Enter 1st marks"))
+b=float(input("Enter 2nd marks"))
+c=float(input("Enter 3rd marks"))
+d=float(input("Enter 4th marks"))
+e=float(input("Enter 5th marks"))
+f=int(a+b+c+d+e)
+avg=f/5;
+percent=(f/500)*100
+print(f,"is total marks")
+print(avg,"is the average marks of student")
+print(percent,"is the total percent of the subject")
