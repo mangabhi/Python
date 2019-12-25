b0d23d91e7a5e3322c93d67325f6816a373650bf
Abhishek kumar manglam <44540425+mangabhi@users.noreply.github.com>
2019-12-25 15:50:04 +0530

    Create Concatenaction.py

---
+++ Concatenaction.py
@@ -0,0 +1,5 @@
+#Define a function that can accept two strings as input and concatenate them and then print it in console.
+a=str(input("Enter a string: "))
+b=str(input("Enter a string: "))
+c=str(a+b)
+print(c)
