73bea808a74d9b8c32100c6034fb7f516e562845
Abhishek kumar manglam <44540425+mangabhi@users.noreply.github.com>
2019-12-25 16:58:58 +0530

    Create factorial_module.py

---
+++ factorial_module.py
@@ -0,0 +1,5 @@
+#Write a program which can compute the factorial of a given numbers. The results should be printed in a comma-separated sequence on a single line. Suppose the following input is supplied to the program: 8 Then, the output should be: 40320
+import math
+n=int(input("Enter a number: "))
+p=math.palindrome(n)
+print(p)
