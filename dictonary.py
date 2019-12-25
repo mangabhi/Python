eb169c1cc84656c396f0d05a825e2e31fea445fd
Abhishek kumar manglam <44540425+mangabhi@users.noreply.github.com>
2019-12-25 17:14:10 +0530

    Create dictonary.py

---
+++ dictonary.py
@@ -0,0 +1,6 @@
+#Define a function which can print a dictionary where the keys are numbers between 1 and 3 (both included) and the values are square of keys.
+d=dict()
+for i in range(1,4):
+    d[i]=i**2
+print(d)
+    
