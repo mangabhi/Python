dca6b85c2ac514f147d100d2cba11b6b892747ee
Abhishek kumar manglam <44540425+mangabhi@users.noreply.github.com>
2020-01-02 21:41:51 +0530

    Create Rev_str.py

---
+++ Rev_str.py
@@ -0,0 +1,5 @@
+#Description : Please write a program which accepts a string from console and print it in reverse order. Example: If the following string is given as input to the program: rise to vote sir Then, the output of the program should be: ris etov ot esir
+
+s=input("Enter a string: ")
+s=s[::-1]
+print(s)
