6d60c3fd309011c5ea3134e2100a395a68f84ce7
Abhishek kumar manglam <44540425+mangabhi@users.noreply.github.com>
2020-01-02 20:27:38 +0530

    Create yes_or_no.py

---
+++ yes_or_no.py
@@ -0,0 +1,6 @@
+#Description : Write a program which accepts a string as input to print "Yes" if the string is "yes" or "YES" or "Yes", otherwise print "No".
+a=str(input("Enter a string: "))
+if a=="YES" or a=="yes":
+    print("Yes")
+else:
+    print("No")
