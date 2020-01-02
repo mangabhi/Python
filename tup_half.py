262410aeb721562b879e424db9c3a6fc614103ed
Abhishek kumar manglam <44540425+mangabhi@users.noreply.github.com>
2020-01-02 19:54:09 +0530

    Create tup_half.py

---
+++ tup_half.py
@@ -0,0 +1,5 @@
+#Description : With a given tuple (1,2,3,4,5,6,7,8,9,10), write a program to print the first half values in one line and the last half values in one line.
+
+a=(1,2,3,4,5,6,7,8,9,10)
+print(a[:5])
+print(a[5:])
