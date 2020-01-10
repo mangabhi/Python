89af1c98ba536e5e02611a32229ea462cdc46e3c
Abhishek kumar manglam <44540425+mangabhi@users.noreply.github.com>
2020-01-10 13:27:24 +0530

    Create Number23.py

---
+++ Number23.py
@@ -0,0 +1,7 @@
+#Description : Write a program that computes the value of a+aa+aaa+aaaa with a given digit as the value of a. Suppose the following input is supplied to the program: 9 Then, the output should be: 11106
+a = input()
+n1 = int( "%s" % a )
+n2 = int( "%s%s" % (a,a) )
+n3 = int( "%s%s%s" % (a,a,a) )
+n4 = int( "%s%s%s%s" % (a,a,a,a) )
+print (n1+n2+n3+n4)
