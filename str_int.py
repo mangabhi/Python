f7bd26835e14dbd028b890d09c6040187a1fbcee
Abhishek kumar manglam <44540425+mangabhi@users.noreply.github.com>
2020-01-11 11:30:19 +0530

    Create str_int.py

---
+++ str_int.py
@@ -0,0 +1,10 @@
+#Define a function that can receive two integral numbers in string form and compute their sum and then print it in console.
+def bac(a,b):
+    c=a+b
+    return c
+
+
+a=str(input())
+b=str(input())
+x=bac(a,b)
+print(x)
