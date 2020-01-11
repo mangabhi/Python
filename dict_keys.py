d1b9c4c4da5e5e58fd94e9e8e31e1080bf17f330
Abhishek kumar manglam <44540425+mangabhi@users.noreply.github.com>
2020-01-11 11:06:22 +0530

    Update dict_keys.py

--- dict_keys.py
+++ dict_keys.py
@@ -3,4 +3,12 @@
 
 for i in range(1,21):
     d={i:i*i}
-    print(list(d.keys()))
+    print(list(d.keys()))                    #for keys 
+
+    
+#Description : Define a function which can generate a dictionary where the keys are numbers between 1 and 20 (both included) and the values are square of keys.
+#The function should just print the values only.
+for i in range(1,21):
+    d={i:i*i}
+    #l.append(d)
+    print(list(d.values()))                 #for values 
