14d56046a5b5cf53ae3f54de6f64476bba6ae283
Abhishek kumar manglam <44540425+mangabhi@users.noreply.github.com>
2020-01-11 11:01:31 +0530

    Create genss_list.py

---
+++ genss_list.py
@@ -0,0 +1,7 @@
+#Define a function which can generate a list where the values are square of numbers between 1 and 20 (both included).
+#Then the function needs to print the last 5 elements in the list.
+l=[]
+for i in range(1,21):
+    d={i:i*i}
+    l.append(d)
+print(l[0:5])
